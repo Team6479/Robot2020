353916417f00d058af0c335010afd1465a6c358c
2stinkysocks <zmehall@gmail.com>
2020-01-24 15:18:43 -0700

    Removed broken import

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -11,7 +11,6 @@ import edu.wpi.first.wpilibj.GenericHID;
 import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
-import frc.robot.commands.MoveIntakeArm;
 import frc.robot.commands.TurnIntakeRollers;
 import frc.robot.subsystems.IntakeArm;
 import frc.robot.subsystems.IntakeRollers;
