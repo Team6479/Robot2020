a0f8f2fced46613585915532cca3bfa81ea4ef86
2stinkysocks <zmehall@gmail.com>
2020-02-01 23:22:31 -0700

    Fixed Intake bug and added some comments

--- src/main/java/frc/robot/commands/Intake.java
+++ src/main/java/frc/robot/commands/Intake.java
@@ -19,40 +19,52 @@ public class Intake extends CommandBase {
   private final IntakeRollers intakeRollers;
   private final IntakeArm intakeArm;
 
+  private boolean hasBegun; // make sure that the arm doesn't immediately stop when started
+
   public Intake(IntakeArm intakeArm, IntakeRollers intakeRollers) {
     this.intakeRollers = intakeRollers;
     this.intakeArm = intakeArm;
     // Use addRequirements() here to declare subsystem dependencies.
     addRequirements(this.intakeArm);
     addRequirements(this.intakeRollers);
+
+    hasBegun = false;
   }
 
   // Called when the command is initially scheduled.
   @Override
   public void initialize() {
     intakeArm.initLimitCounters();
+    hasBegun = false;
   }
 
   // Called every time the scheduler runs while the command is scheduled.
+
+  // Checks which limit switch is currently pressed and moves the arm and rollers based on that (may change later)
   @Override
   public void execute() {
     if(intakeArm.isSet(LimitSwitch.SWITCHOUT)) {
       intakeArm.armIn();
       intakeRollers.rollersOff();
+      hasBegun = true;
     } else if(intakeArm.isSet(LimitSwitch.SWITCHIN)) {
       intakeArm.armOut();
       intakeRollers.rollersOn();
+      hasBegun = true;
     }
   }
 
   // Called once the command ends or is interrupted.
+  // Stop the arm when the switch is hit
   @Override
   public void end(boolean interrupted) {
+    intakeArm.armStop();
   }
 
   // Returns true when the command should end.
+  // Stop command when either switch is pressed
   @Override
   public boolean isFinished() {
-    return intakeArm.isSet(LimitSwitch.SWITCHOUT) || intakeArm.isSet(LimitSwitch.SWITCHIN);
+    return (intakeArm.isSet(LimitSwitch.SWITCHOUT) || intakeArm.isSet(LimitSwitch.SWITCHIN)) && hasBegun;
   }
 }
