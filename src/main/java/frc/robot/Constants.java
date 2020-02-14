0a4f7cd0756b92e726dfc70213b58e878525d369
Aiden Onstott <aidenonstott@gmail.com>
2020-02-13 17:28:30 -0700

    Added auto commands with default values, commands yet to be written are TODOs

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -7,12 +7,15 @@
 
 package frc.robot;
 
+import frc.robot.commands.TurnDrivetrain.Direction;
+
 /**
  * The Constants class provides a convenient place for teams to hold robot-wide numerical or boolean
- * constants.  This class should not be used for any other purpose.  All constants should be
- * declared globally (i.e. public static).  Do not put anything functional in this class.
+ * constants. This class should not be used for any other purpose. All constants should be declared
+ * globally (i.e. public static). Do not put anything functional in this class.
  *
- * <p>It is advised to statically import this class (or one of its inner classes) wherever the
+ * <p>
+ * It is advised to statically import this class (or one of its inner classes) wherever the
  * constants are needed, to reduce verbosity.
  */
 public final class Constants {
@@ -46,10 +49,5 @@ public final class Constants {
     public static final int P = 0;
     public static final int I = 0;
     public static final int D = 0;
-  }
-
-  public static final class AutoConstants {
-    public static final double lineDistance = 0;
-    public static final double lineSpeed = 0;
   }
 }
--- src/main/java/frc/robot/commands/Auto.java
+++ src/main/java/frc/robot/commands/Auto.java
@@ -8,11 +8,11 @@
 package frc.robot.commands;
 
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
-import frc.robot.Constants.AutoConstants;
+import frc.robot.commands.TurnDrivetrain.Direction;
 import frc.robot.subsystems.Drivetrain;
 import frc.robot.subsystems.Turret;
 
-// NOTE:  Consider using this command inline, rather than writing a subclass.  For more
+// NOTE: Consider using this command inline, rather than writing a subclass. For more
 // information, see:
 // https://docs.wpilib.org/en/latest/docs/software/commandbased/convenience-features.html
 public class Auto extends SequentialCommandGroup {
@@ -22,6 +22,21 @@ public class Auto extends SequentialCommandGroup {
   public Auto(Drivetrain drivetrain, Turret turret) {
     // Add your commands in the super() call, e.g.
     // super(new FooCommand(), new BarCommand());
-    addCommands(new StraightDrive(drivetrain, AutoConstants.lineSpeed, AutoConstants.lineDistance), new AimTurret(turret));
+    addCommands(
+        // START OF AUTO 1
+        new StraightDrive(drivetrain, 0, 0),
+        new AimTurret(turret),
+        // TODO: Shooting command
+        // TODO: START OF AUTO 2
+        new TurnDrivetrain(drivetrain, 0, Direction.Left),
+        new StraightDrive(drivetrain, 0, 0),
+        // TODO: Move intake down
+        new TurnDrivetrain(drivetrain, 0, Direction.Left),
+        new StraightDrive(drivetrain, 0, 0),
+        // TODO: Pull up intake
+        // TODO: START OF AUTO 3
+        new AimTurret(turret)
+        // TODO: Shooting command
+    );
   }
 }
