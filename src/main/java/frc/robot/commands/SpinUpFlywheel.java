9b8c3e9d18f8575304537177a997a3f905f33778
Leo Wilson <leowilson.xlw123@gmail.com>
2020-02-15 21:43:58 -0700

    Add skeleton code for port selection

--- src/main/java/frc/robot/commands/SpinUpFlywheel.java
+++ src/main/java/frc/robot/commands/SpinUpFlywheel.java
@@ -29,17 +29,34 @@ public class SpinUpFlywheel extends CommandBase {
   }
 
   /**
-   * @param distance The distance to the target in inches
+   * Checks whether or not the shooter can hit the inner port from its current position
+   * This method may contain magic numbers which are derived from real-world testing
+   * @param distance The distance to the target (in)
+   * @param angle The angle from the target (deg)
    */
-  private double calculate(double distance) {
-    return 0;
+  private boolean checkInnerPort(double distance, double angle) {
+    return (angle >= -180 && angle <= 180) && (distance > 16 && distance < 256);
+  }
+
+  /**
+   * Calculates the ideal flywheel RPM to hit the highest-scoring target possible
+   * This method may contain magic numbers which are derived from regression models of data from real-world testing
+   * @param distance The distance to the target (in)
+   * @param angle The angle from the target (deg)
+   */
+  private double calculate(double distance, double angle) {
+    if (checkInnerPort(distance, angle)) { // target inner port
+      return 0; // TODO: eq for inner port
+    } else { // target outer port
+      return 0; // TODO: eq for outer port
+    }
   }
 
   // Called when the command is initially scheduled.
   @Override
   public void initialize() {
     if (Limelight.hasTarget()) {
-      flywheel.set(calculate(distanceCalculator.calculate(Math.toRadians(Limelight.getYOffset()))));
+      flywheel.set(calculate(distanceCalculator.calculate(Math.toRadians(Limelight.getYOffset())), Limelight.getXOffset()));
     } else {
       // TODO: handle
     }
