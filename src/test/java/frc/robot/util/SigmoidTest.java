4e63327669d3d2ecbdd6dca07cb8a25ed4421d89
Leo Wilson <leowilson.xlw123@gmail.com>
2020-02-19 00:43:01 -0700

    Write more tests

--- src/test/java/frc/robot/util/SigmoidTest.java
+++ src/test/java/frc/robot/util/SigmoidTest.java
@@ -6,10 +6,19 @@ import org.junit.jupiter.api.Test;
 
 class SigmoidTest {
   @Test
-  @DisplayName("1 + 1 = 2")
-  void calculateSigmoid() {
+  @DisplayName("First Sigmoid")
+  void sigmoid0() {
     Sigmoid sigmoid = new Sigmoid(1.0, 1.0, 1.0, true, 0, 0);
-    assertEquals(0.5, sigmoid.calculate(0), "");
-    assertEquals(1, sigmoid.calculate(40), "40 -> 1");
+    assertEquals(0.5, sigmoid.calculate(0), 0.01, "(0, 0.5)");
+    assertEquals(1, sigmoid.calculate(40), 0.01, "(40, 1)");
+  }
+
+  @Test
+  @DisplayName("Second Sigmoid")
+  void simgoid1() {
+    Sigmoid sigmoid = new Sigmoid(2.0, 2.0, 2.0, true, 0, 0);
+    assertEquals(2, sigmoid.calculate(0), 0.01, "(0, 2)");
+    assertEquals(2.49, sigmoid.calculate(1), 0.01, "(1, 2.49)");
+    assertEquals(3.81, sigmoid.calculate(6), 0.01, "(6, 3.81)");
   }
 }
