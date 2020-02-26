d458931bf013484d6d7bffa16efec8298696ab6b
Aiden Onstott <aidenonstott@gmail.com>
2020-02-26 15:02:46 -0700

    Finished autons 1, 2, and 3; Further testing is needed for auton.

--- src/main/java/frc/robot/commands/Auto.java
+++ src/main/java/frc/robot/commands/Auto.java
@@ -7,9 +7,17 @@
 
 package frc.robot.commands;
 
+import edu.wpi.first.wpilibj2.command.InstantCommand;
+import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import edu.wpi.first.wpilibj2.command.WaitCommand;
 import frc.robot.commands.TurnDrivetrain.Direction;
+import frc.robot.subsystems.AlignmentBelt;
 import frc.robot.subsystems.Drivetrain;
+import frc.robot.subsystems.Flywheel;
+import frc.robot.subsystems.Indexer;
+import frc.robot.subsystems.IntakeArm;
+import frc.robot.subsystems.IntakeRollers;
 import frc.robot.subsystems.Turret;
 
 // NOTE: Consider using this command inline, rather than writing a subclass. For more
@@ -19,24 +27,48 @@ public class Auto extends SequentialCommandGroup {
   /**
    * Creates a new Auto.
    */
-  public Auto(Drivetrain drivetrain, Turret turret) {
+  public Auto(Drivetrain drivetrain, Turret turret, Flywheel flywheel, Indexer indexer,
+      AlignmentBelt alignmentBelt, IntakeRollers intakeRollers, IntakeArm intakeArm) {
     // Add your commands in the super() call, e.g.
     // super(new FooCommand(), new BarCommand());
     addCommands(
         // START OF AUTO 1
         new StraightDrive(drivetrain, 0, 0),
         new AimTurret(turret),
-        // TODO: Shooting command
-        // TODO: START OF AUTO 2
+        new SpinUpFlywheel(flywheel),
+        new InstantCommand(indexer::run, indexer),
+        new InstantCommand(alignmentBelt::run, alignmentBelt),
+        new WaitCommand(4), // TODO: Test and refine timing
+        new InstantCommand(alignmentBelt::stop, alignmentBelt),
+        new InstantCommand(indexer::stop, indexer),
+        new InstantCommand(flywheel::off, flywheel),
+        // START OF AUTO 2
         new TurnDrivetrain(drivetrain, 0, Direction.Left),
         new StraightDrive(drivetrain, 0, 0),
-        // TODO: Move intake down
-        new TurnDrivetrain(drivetrain, 0, Direction.Left),
+        new ParallelCommandGroup(new SequentialCommandGroup(new InstantCommand(() -> {
+          if (intakeRollers.getSpeed() > 0) {
+            intakeRollers.rollersOff();
+          } else {
+            intakeRollers.rollersOn();
+          }
+        }, intakeRollers), new ToggleIntakeArm(intakeArm),
+            new TurnDrivetrain(drivetrain, 0, Direction.Left))),
         new StraightDrive(drivetrain, 0, 0),
-        // TODO: Pull up intake
-        // TODO: START OF AUTO 3
-        new AimTurret(turret)
-        // TODO: Shooting command
-    );
+        new ParallelCommandGroup(new SequentialCommandGroup(new InstantCommand(() -> {
+          if (intakeRollers.getSpeed() > 0) {
+            intakeRollers.rollersOff();
+          } else {
+            intakeRollers.rollersOn();
+          }
+        }, intakeRollers), new ToggleIntakeArm(intakeArm))),
+        // START OF AUTO 3
+        new AimTurret(turret),
+        new SpinUpFlywheel(flywheel),
+        new InstantCommand(indexer::run, indexer),
+        new InstantCommand(alignmentBelt::run, alignmentBelt),
+        new WaitCommand(4), // TODO: Test and refine timing
+        new InstantCommand(alignmentBelt::stop, alignmentBelt),
+        new InstantCommand(indexer::stop, indexer),
+        new InstantCommand(flywheel::off, flywheel));
   }
 }
