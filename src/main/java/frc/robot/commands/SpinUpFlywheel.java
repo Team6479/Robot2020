24991f7940d578e2699ac8f4f8be4ae2a2dfe4e3
Leo Wilson <leo@leomwilson.com>
2020-02-28 17:46:23 -0700

    Add math for flywheel based on loose regression equation

--- src/main/java/frc/robot/commands/SpinUpFlywheel.java
+++ src/main/java/frc/robot/commands/SpinUpFlywheel.java
@@ -25,27 +25,13 @@ public class SpinUpFlywheel extends CommandBase {
   }
 
   /**
-   * Checks whether or not the shooter can hit the inner port from its current position
-   * This method may contain magic numbers which are derived from real-world testing
-   * @param distance The distance to the target (in)
-   * @param angle The angle from the target (deg)
-   */
-  private boolean checkInnerPort(double distance, double angle) {
-    return (angle >= -180 && angle <= 180) && (distance > 16 && distance < 256);
-  }
-
-  /**
    * Calculates the ideal flywheel RPM to hit the highest-scoring target possible
    * This method may contain magic numbers which are derived from regression models of data from real-world testing
    * @param distance The distance to the target (in)
    * @param angle The angle from the target (deg)
    */
-  private double calculate(double distance, double angle) {
-    if (checkInnerPort(distance, angle)) { // target inner port
-      return 0; // TODO: eq for inner port
-    } else { // target outer port
-      return 0; // TODO: eq for outer port
-    }
+  private double calculate(double distance) {
+    return (-0.0003607 * Math.pow(distance, 3)) + (1.027 * Math.pow(distance, 2)) + (428.1 * distance) + 88422.6; // based on inner port testing only
   }
 
   // Called when the command is initially scheduled.
@@ -55,8 +41,7 @@ public class SpinUpFlywheel extends CommandBase {
       flywheel.set(calculate(
           distanceCalculator.calculate(
             Math.toRadians(Limelight.getYOffset())
-          ), Limelight.getXOffset()
-        ));
+          )));
     } else {
       // TODO: handle
     }
