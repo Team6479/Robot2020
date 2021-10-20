8b868ba94aa76af5a995df527cf9097eaacee415
Leo Wilson <leo@leomwilson.com>
2021-10-20 09:06:09 -0700

    update turret center position

--- src/main/java/frc/robot/autons/OppositeTrenchPickupAuton.java
+++ src/main/java/frc/robot/autons/OppositeTrenchPickupAuton.java
@@ -43,7 +43,7 @@ public class OppositeTrenchPickupAuton extends SequentialCommandGroup {
       new WaitCommand(0.5),
       new StraightDrive(drivetrain, navX, 0.5, 80),
       new WaitCommand(0.5),
-      new InstantCommand(() -> turret.setPosition(20), turret), // reset turret position
+      new InstantCommand(() -> turret.setPosition(-190), turret), // reset turret position
       new InstantCommand(() -> {
         Limelight.setLEDState(LEDState.Auto);
       }),
--- src/main/java/frc/robot/autons/TrenchPickupAuton.java
+++ src/main/java/frc/robot/autons/TrenchPickupAuton.java
@@ -54,7 +54,7 @@ public class TrenchPickupAuton extends SequentialCommandGroup {
       new WaitCommand(0.5),
       new StraightDrive(drivetrain, navX, 0.5, 40),
       new WaitCommand(0.5),
-      new InstantCommand(() -> turret.setPosition(20), turret), // reset turret position
+      new InstantCommand(() -> turret.setPosition(-190), turret), // reset turret position
       new InstantCommand(() -> {
         Limelight.setLEDState(LEDState.Auto);
       }),
